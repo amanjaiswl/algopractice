19a83344379a96dc56c39c26095d5323b0ff4983
aman <amanvidyaviharian@gmail.com>
2025-03-25 15:20:22 +0530

    solved some questions

--- codeforces/1011div2/4.cpp
+++ codeforces/1011div2/4.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n, k;
+  cin >> n >> k;
+
+  priority_queue<int> Q;
+
+  ll ans = 0;
+
+  for (int i = 0; i < n; i++) {
+    int d;
+    cin >> d;
+    Q.push(d);
+    if ((n - i) % (k + 1) == 0) {
+      ans += Q.top();
+      Q.pop();
+    }
+  }
+
+  cout << ans << '\n';
+}
 
 int main() {
   ios::sync_with_stdio(false);
